7b1445dbe76504eef2da167e7b38b5072e8c37fe
Sadab Hafiz <sadabhafizny@gmail.com>
2023-08-20 14:25:04 -0400

    feat: change ring color and click to toggle cell

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   const [setup, setSetup] = useState(false);
-  const [size, setSize] = useState(6);
-  // TODO CHANGE THE SIZE SELECTOR TO A COLOR BAR SINCE FLEX MAKES IT REDUNDANT
+  const [color, setColor] = useState("border-green-300");
   const [row, setRow] = useState(0);
   const [col, setCol] = useState(0);
   const [grid, setGrid] = useState(Array(row).fill(Array(col).fill(0)));
 
   useEffect(()=>{
-    setGrid(Array(row).fill(Array(col).fill(0)));
+    setGrid(JSON.parse(JSON.stringify(Array(row).fill(Array(col).fill(0)))));
+    // TODO: COPY OLD STATES WHEN THE ROW AND COL CHANGES
   },[row,col])
 
   return (
@@ -32,10 +32,14 @@ export default function App() {
                 <input type="number" id="quantity" name="quantity" defaultValue={0} min="0" max="50" onChange={e => setCol(+e.target.value)} className="rounded-r-md border-black border-y-2 border-r-2 shadow-md text-center text-lg font-md"/>
               </div>
               <div className="grow"></div>
-              <select id="sizeSelector" defaultValue={5} onChange={e => setSize(+e.target.value)} className="py-2 text-center text-md md:text-lg lg:text-xl font-electrolize text-black block font-bold uppercase bg-green-100 border-2 border-black hover:border-white shadow leading-tight focus:outline-none focus:shadow-outline rounded-md hover:bg-black hover:text-green-300 focus:bg-black focus:text-green-300">
-                  <option value={5}>Small</option>
-                  <option value={10}>Medium</option>
-                  <option value={20}>Large</option>
+              <select id="colorSelector" defaultValue="green" onChange={e => setColor(e.target.value)} className="py-2 text-center text-md md:text-lg lg:text-xl font-electrolize text-black block font-bold uppercase bg-green-100 border-2 border-black hover:border-white shadow leading-tight focus:outline-none focus:shadow-outline rounded-md hover:bg-black hover:text-green-300 focus:bg-black focus:text-green-300">
+                  <option value="border-green-300">Green</option>
+                  <option value="border-red-300">Red</option>
+                  <option value="border-blue-300">Blue</option>
+                  <option value="border-orange-300">Orange</option>
+                  <option value="border-yellow-300">Yellow</option>
+                  <option value="border-indigo-300">Indigo</option>
+                  <option value="border-violet-300">Violet</option>
               </select>
               <button onClick={() => setSetup(true)} className="relative inline-flex items-center justify-center px-6 py-2 overflow-hidden font-electrolize font-bold text-md md:text-lg lg:text-xl uppercase text-green-300 transition duration-300 ease-out border-2 border-white rounded-md bg-black shadow-md group">
                 <span className="absolute inset-0 flex items-center justify-center w-full h-full text-green-300 duration-300 -translate-x-full bg-black group-hover:translate-x-0 ease">
@@ -51,8 +55,27 @@ export default function App() {
         {
             grid.map((r,i:any) => (
               <div key={i} className="flex justify-center">
-                {/* TODO: CHANGE VALUES AND COLOR ONCLICK */}
-                {r.map((cell:any,j:any) => <div key={j} className={` aspect-square grow bg-${cell>0?"black":"white"} border-2 border-green-300 ring-1 ring-black`}/>)}
+                {
+                  r.map((_:any,j:any) => 
+                    <div 
+                      key={j} 
+                      onClick={
+                        () => {
+                          let newGrid=[...grid];
+                          if(grid[i][j]==0){
+                            newGrid[i][j] = 1;
+                          }
+                          else{
+                            newGrid[i][j] = 0;
+                          }
+                          setGrid(newGrid);
+                        }
+                      }
+                      className={`aspect-square grow ${color} border-2 ring-1 ring-black ${grid[i][j]>0?"bg-black":"bg-white"}`}
+                      //TODO: MOUSE CLICK DRAG EVENT
+                    />
+                  )
+                }
               </div>
             ))
         }
