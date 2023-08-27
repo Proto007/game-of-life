2b7129a18afb304b0c086031da6e43097eb921be
Sadab Hafiz <sadabhafizny@gmail.com>
2023-08-27 13:34:13 -0400

    feat: add back button after game start

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState,useEffect } from "react";
-import {GiPerspectiveDiceSixFacesRandom} from "react-icons/gi"
-import {AiOutlineClear} from "react-icons/ai"
+import { GiPerspectiveDiceSixFacesRandom } from "react-icons/gi"
+import { AiOutlineClear,AiOutlineArrowRight,AiOutlineArrowLeft } from "react-icons/ai"
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -56,6 +56,13 @@ export default function App() {
       { 
         setup ?
           <div className="bg-green-300 p-2 flex-col md:flex-row flex justify-center gap-2 border-b border-black">
+            <button onClick={() => setSetup(false)} className="relative inline-flex items-center justify-center px-6 py-2 overflow-hidden font-electrolize font-bold text-md md:text-lg lg:text-xl uppercase text-green-300 transition duration-300 ease-out border-2 border-white rounded-md bg-black shadow-md group">
+              <span className="absolute inset-0 flex items-center justify-center w-full h-full text-green-300 duration-300 -translate-x-full bg-black group-hover:translate-x-0 ease">
+                <AiOutlineArrowLeft size={30}/>
+              </span>
+              <span className="absolute flex items-center justify-center w-full h-full text-green-300 transition-all duration-300 transform group-hover:translate-x-full ease">Back</span>
+              <span className="relative invisible">Back</span>
+            </button>
             <div className="flex font-electrolize align-middle w-full md:w-auto">
               <div className="grow md:grow-0 items-center justify-center px-6 py-2 overflow-hidden font-electrolize font-bold text-md md:text-lg lg:text-xl uppercase text-black border-2 border-black rounded-l-md bg-green-100 shadow-md">Population</div>
               <div className="rounded-r-md border-black border-y-2 border-r-2 shadow-md text-center text-lg font-md px-6 py-2 bg-white font-bold">{population}</div>
@@ -98,7 +105,7 @@ export default function App() {
               </select>
               <button onClick={() => setSetup(true)} className="relative inline-flex items-center justify-center px-6 py-2 overflow-hidden font-electrolize font-bold text-md md:text-lg lg:text-xl uppercase text-green-300 transition duration-300 ease-out border-2 border-white rounded-md bg-black shadow-md group">
                 <span className="absolute inset-0 flex items-center justify-center w-full h-full text-green-300 duration-300 -translate-x-full bg-black group-hover:translate-x-0 ease">
-                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                  <AiOutlineArrowRight size={30}/>
                 </span>
                 <span className="absolute flex items-center justify-center w-full h-full text-green-300 transition-all duration-300 transform group-hover:translate-x-full ease">Start</span>
                 <span className="relative invisible">Start</span>
